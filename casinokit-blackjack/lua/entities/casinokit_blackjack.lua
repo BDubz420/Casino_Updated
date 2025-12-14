3c91bfd54f71ec0ca4c4e2ff82c4713296cef7cf
BDubz420 <129692319+BDubz420@users.noreply.github.com>
2025-12-14 02:35:57 -0500

    Add files via upload

---
+++ casinokit-blackjack/lua/entities/casinokit_blackjack.lua
@@ -0,0 +1,50 @@
+AddCSLuaFile()
+
+local STOP_HARD = 0
+local STOP_SOFT = 1
+
+ENT.Spawnable = true
+ENT.Category = "Casino Kit"
+ENT.PrintName = "Blackjack table"
+
+DEFINE_BASECLASS("casinokit_table")
+ENT.Base = "casinokit_table"
+
+ENT.GameClass = "Blackjack"
+
+function ENT:SetupDataTables()
+	BaseClass.SetupDataTables(self)
+
+	self:NetworkVar("Int", 4, "MinBet")
+	self:NetworkVar("Int", 5, "MaxBet")
+	self:NetworkVar("Int", 6, "TimeoutDelay")
+	self:NetworkVar("Int", 7, "StopAt")
+end
+
+function ENT:Initialize()
+	if self:GetMinBet() == 0 then
+		self:SetMinBet(5)
+		self:SetMaxBet(1e4) 
+		self:SetTimeoutDelay(15)
+		self:SetStopAt(STOP_SOFT)
+	end
+
+	BaseClass.Initialize(self)
+end
+
+
+function ENT:OnGameConfigReceived(key, value)
+	if key == "minbet" then
+		assert(type(value) == "number" and value > 0)
+		self:SetMinBet(value)
+	elseif key == "maxbet" then
+		assert(type(value) == "number" and value > 0)
+		self:SetMaxBet(value)
+	elseif key == "timeoutdelay" then
+		assert(type(value) == "number" and value > 0)
+		self:SetTimeoutDelay(value)
+	elseif key == "stopat" then
+		assert(type(value) == "number" and (value >= 0 or value <= 1))
+		self:SetStopAt(value)
+	end
+end
